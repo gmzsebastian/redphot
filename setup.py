e8fbc3ac6c4d8ccebce0c266f10a700000ed5a02
Sebastian Gomez <gmz.sebastian@gmail.com>
2026-08-20 11:43:13 -0500

    Package version updates

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@ from setuptools import setup
 
 setup(
     name='redphot',
-    version='0.1',
+    version='0.1.0',
     author='Sebastian Gomez',
     author_email='sebastian.gomez@austin.utexas.edu',
     description='Functions to reduce photometry.',
     url='https://github.com/gmzsebastian/redphot',
     license='MIT License',
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     packages=['redphot'],
     include_package_data=True,
     package_data={'redphot': ['ref_data/*']},
     install_requires=[
-        'numpy',
-        'matplotlib',
-        'astropy',
-        'astroquery',
-        'photutils',
-        'scipy',
-        'scikit-image',
-        'emcee'
-    ]
+        'numpy>=1.23',
+        'matplotlib>=3.6',
+        'astropy>=5.2',
+        'astroquery>=0.4.6',
+        'photutils>=1.9',
+        'scipy>=1.9',
+    ],
+    extras_require={
+        'cosmic_rays': ['astroscrappy>=1.1'],
+    },
 )
